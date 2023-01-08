49a10551c3456ad4bd066af734bab8646bfbf68a
Patrick Bauer <patrick.jean.bauer@googlemail.com>
2023-01-08 19:07:00 +0100

    Load items from compendia

--- src/module/actor/NOVA6Actor.ts
+++ src/module/actor/NOVA6Actor.ts
@@ -7,10 +7,37 @@ import defaultItems from "../../../system/data/en/default.json";
 
 export class NOVA6Actor extends Actor {
     /** @override */
-    protected _onCreate(data, options, userId) {
+    protected async _onCreate(data, options, userId) {
         super._onCreate(data, options, userId);
 
-        this.createEmbeddedDocuments("Item", defaultItems.content);
+        if (data.items.length) {
+            return;
+        }
+
+        // Add all default items
+        const itemsToCreate: any = defaultItems.content;
+
+        // @ts-ignore
+        const aspects = await game?.packs.get("nova6.aspects").getDocuments();
+        for (const aspectName of defaultItems.packs.aspects) {
+            const packItem = aspects.find((aspect) => aspect.name === aspectName);
+
+            if (packItem) {
+                itemsToCreate.push(packItem);
+            }
+        }
+
+        // @ts-ignore
+        const assets = await game?.packs.get("nova6.assets").getDocuments();
+        for (const assetName of defaultItems.packs.assets) {
+            const packItem = assets.find((aspect) => aspect.name === assetName);
+
+            if (packItem) {
+                itemsToCreate.push(packItem);
+            }
+        }
+
+        this.createEmbeddedDocuments("Item", itemsToCreate);
     }
 }
 
